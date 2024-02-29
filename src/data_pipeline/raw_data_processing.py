706ccdf6a523650ba910dd2925bd8fcfd4d43b90
Bart van Woesik <bartwoesik1@gmail.com>
2024-02-29 15:37:48 +0100

    refactor raw data pipeline

--- src/data_pipeline/raw_data_processing.py
+++ src/data_pipeline/raw_data_processing.py
@@ -4,6 +4,7 @@ import pandas as pd
 import os
 import hydra
 from hydra.utils import instantiate
+from typing import List, Dict
 from my_logger.custom_logger import logger
 
 
@@ -59,72 +60,109 @@ def create_aggration_dataframe(cfg: dict, df: pd.DataFrame) -> pd.DataFrame:
     return df.groupby(ID).agg(**aggregation_specs).reset_index()
 
 
-def get_orderd_data_files(data_path: Path, all_file_sources):
-    file_dict = []
-    all_files = os.listdir(data_path)
-    file_dict = [
-        {source: [file for file in all_files if source in file and file is not None]}
-        for source in all_file_sources
-    ]
+def get_orderd_data_files(data_path: Path, all_file_sources) -> Dict[str, List[str]]:
+    """
+    Retrieve a dictionary of ordered data files based on the given data path and file sources.
 
-    return file_dict
+    Args:
+        data_path (Path): The path to the directory containing the data files.
+        all_file_sources (list): A list of file sources to filter the data files.
+
+    Returns:
+        list: A list of dictionaries, where each dictionary contains a file source as the key and a list of corresponding
+        data files as the value.
+    """
+    all_files = os.listdir(data_path)
+    return {
+        source: [file for file in all_files if source in file and file is not None]
+        for source in all_file_sources
+    }
+
+
+def read_file(cfg_columns: List, unique_cols: List, file: str) -> pd.DataFrame:
+    """
+    Read a parquet file and return a DataFrame.
+
+    Args:
+        cfg_columns (List): List of configuration columns.
+        unique_cols (List): List of unique columns.
+        file (str): Path to the parquet file.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the data from the parquet file.
+    """
+    if len(cfg_columns.time_col) > 0:
+        return pd.read_parquet(
+            DATA_PATH / file,
+            columns=[ID] + unique_cols + [cfg_columns.time_col[0]],
+        )
+    else:
+        return pd.read_parquet(DATA_PATH / file, columns=[ID] + unique_cols)
 
 
 @hydra.main(config_path="../../", config_name="config.yaml")
-def create_dataframe(cfg: DictConfig) -> pd.DataFrame:
+def create_dataframe(cfg: DictConfig, split: str) -> pd.DataFrame:
+    """
+    Create a dataframe by reading and joining multiple files based on the provided configuration.
+
+    Args:
+        cfg (DictConfig): Configuration object containing data sources and columns.
+
+    Returns:
+        pd.DataFrame: The resulting dataframe.
+    """
+
     # all_sources we want to use
     all_file_sources = list(cfg.data.keys())
-    print(type(all_file_sources))
     data_structure = get_orderd_data_files(DATA_PATH, all_file_sources=all_file_sources)
-    logger.info(f"Data structure: {data_structure}")
-    # Read base file where we can join the other files on
-    df_base = pd.read_parquet(DATA_PATH / BASE_FILE)
 
     # Only keep the ID and TARGET column
-    if TARGET in df_base.columns and ID in df_base.columns:
-        df = pd.DataFrame(df_base[[ID, TARGET]])
+    if split == "train":
+        df_base = pd.read_parquet(
+            DATA_PATH / BASE_FILE, columns=[ID, TARGET, DATE_DECISION]
+        )
     else:
-        df = pd.DataFrame(df_base[[ID]])
+        df_base = pd.read_parquet(DATA_PATH / BASE_FILE, columns=[ID, DATE_DECISION])
+    df = df_base.copy()
 
     # Loop over all the files and join them to the base file
-    for source_file in data_structure:
-        logger.info(f"source_file: {source_file}")
-        source_name = list(source_file.keys())[0]
-        files = list(source_file.values())[0]
-        cfg_columns = cfg.data[source_name]
+    for source, files in data_structure.items():
+        cfg_columns = cfg.data[source]
         df_all_files = pd.DataFrame()
+        unique_cols = list(set(col.name for col in cfg_columns.agg_columns))
+        # Read splitted files and join them to the base file
         for file in files:
-            print(file)
-            unique_cols = list(set([col.name for col in cfg_columns.agg_columns]))
-            if len(cfg_columns.time_col) > 0:
-                df_file = pd.read_parquet(
-                    DATA_PATH / file,
-                    columns=[ID] + unique_cols + [cfg_columns.time_col[0]],
-                )
-            else:
-                df_file = pd.read_parquet(DATA_PATH / file, columns=[ID] + unique_cols)
-            df_all_files = pd.concat([df_all_files, df_file])
+            df_all_files = pd.concat(
+                [df_all_files, read_file(cfg_columns, unique_cols, file)]
+            )
+
         df_combined = df_base.merge(
             df_all_files, on=ID, how="left", validate="one_to_many"
         )
-        logger.info(f"columns in combined dataset: {df_combined.columns}")
-        df_agg = create_aggration_dataframe(cfg_columns, df_combined)
-        logger.info(f"columns in base df: {df.columns}")
-        df = df.merge(df_agg, on="case_id", how="left", validate="one_to_many")
+        df = df.merge(
+            create_aggration_dataframe(cfg_columns, df_combined),
+            on="case_id",
+            how="left",
+            validate="one_to_many",
+        )
 
     # Write the dataframe to feather
-    print("Write df to feather")
+    logger.info("Write df to feather")
     df.to_feather(DATA_PATH / DATA_LOCATION)
 
 
 def main():
+    """
+    This function is the entry point of the raw data processing pipeline.
+    It processes the train and test data by creating dataframes and saving them in the specified locations.
+    """
     global DATA_PATH, DATA_LOCATION, BASE_FILE
     for split in ["train", "test"]:
         logger.info(f"Processing {split} data")
         DATA_PATH = Path(os.getcwd()) / f"data/parquet_files/{split}"
         DATA_LOCATION = f"processed_{split}.feather"
         BASE_FILE = f"{split}_base.parquet"
-        create_dataframe()
+        create_dataframe(split)
 
 
 if __name__ == "__main__":
